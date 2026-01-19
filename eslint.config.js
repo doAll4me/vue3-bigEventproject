24cd792361b37e9ca89ecfd486a8f117e3fe7405
ee <1176187361@qq.com>
2026-01-19 20:14:53 +0800

    0119-项目初始化

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import js from '@eslint/js'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import pluginVue from 'eslint-plugin-vue'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import globals from 'globals'
+
+export default defineConfig([
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{vue,js,mjs,jsx}'],
+  },
+
+  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+    },
+  },
+    {
+    rules: {
+      'vue/multi-word-component-names': [
+        'warn',
+        { ignores: ['index'] }
+      ],
+      'vue/no-setup-props-destructure': 'off',
+      'no-undef': 'error'
+    }
+  },
+
+  js.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+
+  skipFormatting,
+])
